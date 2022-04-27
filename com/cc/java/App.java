0719774da98075b215a29c7643abfcddb456e160
PKNQSC <th_alhakkak@yahoo.de>
2022-04-27 13:03:23 +0200

    cat

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,8 +1,29 @@
 package com.cc.java;
 
+import java.util.ArrayList;
+
 public class App {
     
     public static void main(String[] args) {
+
+       
+        // Cat cat = new Cat("Grizabella");
+        // Cat cat = new Cat("Grizabella");
+        // Cat cat = new Cat("Grizabella");
+        // Cat cat = new Cat("Grizabella");
+
+        String [] nameArr = {"Grizabella","Rum Tum Tugger","Alonzo","Suzanne"};
+        ArrayList <Cat> ObjList = new ArrayList<>();
+
+    
+        for (int i = 0; i < nameArr.length; i++) {
+            // new Cat(nameArr[i]);
+            ObjList.add(new Cat(nameArr[i]));
+
+            
+        }
+        output("Cats: " + ObjList);
+        output(ObjList.get(0).getAttributes());
      
     }
 
